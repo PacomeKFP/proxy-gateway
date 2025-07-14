aa44cecfc4e44fd1ea1d9de0a34fa177f6dd107a
PacomeKFP <pacomekengafe@gmail.com>
2025-07-14 17:55:42 +0100

    Initial Commit: simple gateway for http server allowing all methods

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Request
+import httpx
+
+app = FastAPI()
+
+@app.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
+async def proxy(path: str, request: Request):
+    target_url = f"http://localhost:8002/{path}"
+
+    async with httpx.AsyncClient() as client:
+        response = await client.request(
+            method=request.method,
+            url=target_url,
+            headers=request.headers.raw,
+            content=await request.body()
+        )
+
+    return response.text
